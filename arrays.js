759b5acb037c2d6575e244a746d652fc74fd8eff
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-16 14:52:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,13 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
 var addElementToBeginningOfArray = (chocolateBars,element) =>{
+  return  [element,...chocolateBars] 
+}
+
+var destructivelyAddElementToBeginningOfArray = (chocolateBars,element) =>{
+  return chocolateBars.unshift()
+}
+
+var addElementToEndOfArray = () =>{
   
 }
