381fc2098fd4c5123b2476b53fe95efefbaed799
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-16 14:56:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,25 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
-var addElementToBeginningOfArray = (chocolateBars,element) =>{
-  return  [element,...chocolateBars] 
+var addElementToBeginningOfArray = (chocolateBars1,element) =>{
+  return  [element,...chocolateBars1] 
 }
 
-var destructivelyAddElementToBeginningOfArray = (chocolateBars,element) =>{
-  return chocolateBars.unshift()
+var destructivelyAddElementToBeginningOfArray = (chocolateBars1,element) =>{
+  return chocolateBars1.unshift()
 }
 
-var addElementToEndOfArray = (chocolateBars,element) =>{
-  return [...chocolateBars,element]
+var addElementToEndOfArray = (chocolateBars1,element) =>{
+  return [...chocolateBars1,element]
 }
 
-var destructivelyAddElementToEndOfArray = (chocolateBars,element) =>{
-  return chocolateBars.push()
+var destructivelyAddElementToEndOfArray = (chocolateBars1,element) =>{
+  return chocolateBars1.push()
 }
 
-var 
+var accessElementInArray = (chocolateBars1,index) =>{
+  return chocolateBars1[index]
+}
+
+var destructivelyRemoveElementFromBeginningOfArray = () =>{
+  
+}
